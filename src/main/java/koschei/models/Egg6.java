1b526a772b59c117b51422d15adb95c71d03083c
Andrey Pechenkin <mcreature@inbox.ru>
2023-02-27 20:55:51 +0300

    Egg6.class выполнен без @Component

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,16 +1,23 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Egg6 {
 
-    @Autowired
-    private Needle7 needle7;
+    private Needle7 needle;
+
+    public Egg6(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return "в утке — яйцо, " + needle7.toString();
+        return "в утке — яйцо, " + needle.toString();
     }
 }
